6db88eb953d6267a0c14e1f6e5d21ac081e0b700
andrea813 <andreaburgos813@gmail.com>
2025-04-13 00:02:44 -0300

    ejercicio 1,4,5 y 9 resuelto por Burgos Andrea

--- src/ejercicio1.js
+++ src/ejercicio1.js
@@ -1,3 +1,7 @@
+//1.Crear una variable denominada integrantes y asignarle la cadena de texto con los 
+//nombres de los integrantes del grupo. Mostrar por consola el siguiente texto:  
+//Somos (el valor de la variable) y estamos aprendiendo javascript.
+
 let integrantes = "Gonzalo, Andrea, Lautaro, Martin, Leandro";
 
 console.log("Somos: " + integrantes + "y estamos aprendiendo JavaScript.");
